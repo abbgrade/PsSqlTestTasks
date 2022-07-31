5087c293872a25e99213050ae9cfc568746d1c78
Steffen Kampmann <steffen.kampmann@gmail.com>
2022-07-31 10:06:59 +0200

    added submodule

--- WideWorldImporters.Tasks.ps1
+++ WideWorldImporters.Tasks.ps1
@@ -1,41 +1,51 @@
 
-[System.IO.DirectoryInfo] $SqlServerSamplesDirectory = "$PSScriptRoot\..\..\test\sql-server-samples"
-[string] $WideWorldImportersSsdtRelativePath = 'samples/databases/wide-world-importers/wwi-ssdt/wwi-ssdt'
-[System.IO.DirectoryInfo] $WideWorldImportersSsdtDirectory = Join-Path $SqlServerSamplesDirectory $WideWorldImportersSsdtRelativePath
-[System.IO.FileInfo] $WideWorldImportersProject = Join-Path $SqlServerSamplesDirectory $WideWorldImportersSsdtRelativePath "WideWorldImporters.sqlproj"
-[System.IO.FileInfo] $WideWorldImportersDacPac = Join-Path $SqlServerSamplesDirectory $WideWorldImportersSsdtRelativePath "bin\Debug\WideWorldImporters.dacpac"
+[System.IO.DirectoryInfo] $Script:SqlServerSamplesDirectory = "$PSScriptRoot\..\..\test\sql-server-samples"
+[string] $Script:WideWorldImportersSsdtRelativePath = 'samples/databases/wide-world-importers/wwi-ssdt/wwi-ssdt'
+[System.IO.DirectoryInfo] $Script:WideWorldImportersSsdtDirectory = Join-Path $Script:SqlServerSamplesDirectory $Script:WideWorldImportersSsdtRelativePath
+[System.IO.FileInfo] $Script:WideWorldImportersProject = Join-Path $Script:SqlServerSamplesDirectory $Script:WideWorldImportersSsdtRelativePath "WideWorldImporters.sqlproj"
+[System.IO.FileInfo] $Script:WideWorldImportersDacPac = Join-Path $Script:SqlServerSamplesDirectory $Script:WideWorldImportersSsdtRelativePath "bin\Debug\WideWorldImporters.dacpac"
+[string] $Script:RepoUrl = 'https://github.com/microsoft/sql-server-samples.git'
 
-task PsSqlTestTasks.WideWorldImporters.DacPac.Clean -If { $SqlServerSamplesDirectory.Exists } -Jobs {
-    Remove-Item $SqlServerSamplesDirectory -Recurse -Force
+task PsSqlTestTasks.WideWorldImporters.DacPac.Clean -If { $Script:SqlServerSamplesDirectory.Exists } -Jobs {
+    Remove-Item $Script:SqlServerSamplesDirectory -Recurse -Force
 }
 
-task PsSqlTestTasks.WideWorldImporters.DacPac.AddSolution -If { -Not $SqlServerSamplesDirectory.Exists } -Jobs {
-    New-Item $SqlServerSamplesDirectory -ItemType Directory -ErrorAction Continue
+task PsSqlTestTasks.WideWorldImporters.DacPac.AddSolution -If { -Not $Script:SqlServerSamplesDirectory.Exists } -Jobs {
+    New-Item $Script:SqlServerSamplesDirectory -ItemType Directory -ErrorAction Continue
 }
 
-task PsSqlTestTasks.WideWorldImporters.DacPac.InitSolution -If { -Not ( Test-Path "$SqlServerSamplesDirectory\.git" ) } -Jobs PsSqlTestTasks.WideWorldImporters.DacPac.AddSolution, {
-    Push-Location $SqlServerSamplesDirectory
+task PsSqlTestTasks.WideWorldImporters.DacPac.InitSolution -If { -Not ( Test-Path "$Script:SqlServerSamplesDirectory\.git" ) } -Jobs PsSqlTestTasks.WideWorldImporters.DacPac.AddSolution, {
+    # register remote repository
+    Push-Location $Script:SqlServerSamplesDirectory
     exec { git init }
-    exec { git remote add origin -f https://github.com/microsoft/sql-server-samples.git }
+    exec { git remote add origin -f $Script:RepoUrl }
     Pop-Location
 }
 
-task PsSqlTestTasks.WideWorldImporters.DacPac.CheckoutSolution -If { -Not $WideWorldImportersSsdtDirectory.Exists } -Jobs PsSqlTestTasks.WideWorldImporters.DacPac.InitSolution, {
-    Push-Location $SqlServerSamplesDirectory
+task PsSqlTestTasks.WideWorldImporters.DacPac.CheckoutSolution -If { -Not $Script:WideWorldImportersSsdtDirectory.Exists } -Jobs PsSqlTestTasks.WideWorldImporters.DacPac.InitSolution, {
+    Push-Location $Script:SqlServerSamplesDirectory
+    # configure sparse checkout
     exec { git config core.sparseCheckout true }
-    Set-Content .git/info/sparse-checkout $WideWorldImportersSsdtRelativePath
+    Set-Content .git/info/sparse-checkout $Script:WideWorldImportersSsdtRelativePath
+
+    # download content
     exec { git checkout master }
     Pop-Location
+
+    # register submodule
+    Push-Location $Script:SqlServerSamplesDirectory.Parent
+    exec { git submodule add $Script:RepoUrl }
+    Pop-Location
 }
 
-task PsSqlTestTasks.WideWorldImporters.DacPac.Create -If { -Not $WideWorldImportersDacPac.Exists } -Jobs PsSqlTestTasks.WideWorldImporters.DacPac.CheckoutSolution, {
+task PsSqlTestTasks.WideWorldImporters.DacPac.Create -If { -Not $Script:WideWorldImportersDacPac.Exists } -Jobs PsSqlTestTasks.WideWorldImporters.DacPac.CheckoutSolution, {
     # # can be enabled if dotnet core build is public and working
-    # exec { dotnet build "$SqlServerSamplesDirectory\samples\databases\wide-world-importers\wwi-ssdt\wwi-ssdt\WideWorldImporters.sqlproj" /p:NetCoreBuild=true }
+    # exec { dotnet build $Script:WideWorldImportersProject /p:NetCoreBuild=true }
 
-    assert $WideWorldImportersProject
-    Write-Verbose "WideWorldImportersProject: $WideWorldImportersProject"
+    assert $Script:WideWorldImportersProject
+    Write-Verbose "WideWorldImportersProject: $Script:WideWorldImportersProject"
 
-    Invoke-MsBuild $WideWorldImportersProject
+    Invoke-MsBuild $Script:WideWorldImportersProject
 
-    assert ( Test-Path $WideWorldImportersDacPac )
+    assert ( Test-Path $Script:WideWorldImportersDacPac )
 }
